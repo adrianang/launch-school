423e85990683599744c02d2c40befa7cb8467c81
Adrian Ang <adriancbang@gmail.com>
2022-07-23 22:56:45 -0700

    Refactor calculator

--- js101/lesson-02/calculator.js
+++ js101/lesson-02/calculator.js
@@ -6,26 +6,54 @@
 
 const rlSync = require('readline-sync');
 
-console.log('Welcome to Calculator!');
-console.log("What's the first number?");
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+prompt('Welcome to Calculator!');
+prompt("What's the first number?");
 let number1 = rlSync.question();
 
-console.log("What's the second number?");
+while (invalidNumber(number1)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number1 = rlSync.question();
+}
+
+prompt("What's the second number?");
 let number2 = rlSync.question();
 
-console.log('What operation would you like to perform?\n' +
-            '1) Add 2) Subtract 3) Multiply 4) Divide')
+while (invalidNumber(number2)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number2 = rlSync.question();
+}
+
+prompt('What operation would you like to perform?\n' +
+       '   1) Add 2) Subtract 3) Multiply 4) Divide');
 let operation = rlSync.question();
 
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Must choose 1, 2, 3, or 4');
+  operation = rlSync.question();
+}
+
 let output;
-if (operation === '1') {
-  output = Number(number1) + Number(number2);
-} if (operation === '2') {
-  output = Number(number1) - Number(number2);
-} if (operation === '3') {
-  output = Number(number1) * Number(number2);
-} if (operation === '4') {
-  output = Number(number1) / Number(number2);
+switch (operation) {
+  case '1':
+    output = Number(number1) + Number(number2);
+    break;
+  case '2':
+    output = Number(number1) - Number(number2);
+    break;
+  case '3':
+    output = Number(number1) * Number(number2);
+    break;
+  case '4':
+    output = Number(number1) / Number(number2);
+    break;
 }
 
 console.log(`The result is: ${output}`);
