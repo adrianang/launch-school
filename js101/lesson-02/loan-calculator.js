f0869e509e222feb53d06c0adfcd67bf546266ff
Adrian Ang <adriancbang@gmail.com>
2022-08-27 23:25:03 -0700

    Add helper functions for calculating monthly payment

--- js101/lesson-02/loan-calculator.js
+++ js101/lesson-02/loan-calculator.js
@@ -1,28 +1,67 @@
 const rlSync = require('readline-sync');
-
 let monthlyPayment;
-let loanAmount;
-let annualPercentageRate;
-let loanDurationInYears;
 
-console.log('Welcome to Loan Calculator!');
-console.log('What is your loan amount?');
-loanAmount = Number(rlSync.question());
-console.log(`What is the annual percentage rate (APR) of your loan? (Enter in %)`);
-annualPercentageRate = Number(rlSync.question()) / 100;
-console.log('How many years is your loan\'s term for?');
-loanDurationInYears = Number(rlSync.question());
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function getLoanAmountFromUser() {
+  prompt('What is your loan amount?');
+  let loanAmount = rlSync.question('$');
+  loanAmount = loanAmount.replaceAll(',', '');
+
+  while (invalidNumericInput(loanAmount)) {
+    prompt(`That doesn't seem to be a valid loan amount. Please enter an amount larger than $0.`);
+    loanAmount = rlSync.question('$');
+    loanAmount = loanAmount.replaceAll(',', '');
+  }
+
+  return Number(loanAmount);
+}
+
+function getAPRFromUser() {
+  prompt(`What is the annual percentage rate (APR) of your loan? (Enter in %)`);
+  let annualPercentageRate = rlSync.question();
+
+  while (invalidNumericInput(annualPercentageRate)) {
+    prompt(`That doesn't seem to be a valid rate. Please enter a rate larger than 0. (Enter in %)`);
+    annualPercentageRate = rlSync.question();
+  }
+
+  return (Number(annualPercentageRate) / 100);
+}
+
+function getLoanDurationFromUser() {
+  prompt('How many years is your loan\'s term for?');
+  let loanDurationInYears = rlSync.question();
+
+  while (invalidNumericInput(loanDurationInYears)) {
+    prompt(`That doesn't seem to be a valid loan length. Please enter a duration larger than 0 years.`);
+    loanDurationInYears = rlSync.question();
+  }
+
+  return Number(loanDurationInYears);
+}
+
+function invalidNumericInput(numInput) {
+  return numInput.trimStart() === '' || Number.isNaN(Number(numInput)) || !(Number(numInput) > 0);
+}
+
+function calculateMonthlyPayment(loanAmt, monthlyIntRate, loanDurationInMo) {
+  monthlyPayment = loanAmt *
+                   (monthlyIntRate /
+                   (1 - Math.pow((1 + monthlyIntRate), (-loanDurationInMo))));
+
+  return monthlyPayment;
+}
+
+console.clear();
+prompt('Welcome to Loan Calculator!');
+let loanAmount = getLoanAmountFromUser();
+let annualPercentageRate = getAPRFromUser();
+let loanDurationInYears = getLoanDurationFromUser();
 let monthlyInterestRate = annualPercentageRate / 12;
 let loanDurationInMonths = loanDurationInYears * 12;
-
-monthlyPayment = loanAmount * (monthlyInterestRate / (1 - Math.pow((1 + monthlyInterestRate), (-loanDurationInMonths))));
-
-console.log(`Your monthly payment for a loan of $${loanAmount} with an APR of ${annualPercentageRate * 100}% for ${loanDurationInYears} years is:`);
-console.log(`$${monthlyPayment.toFixed(2)}`);
-
-// PSEUDOCODE
-// Ask user for loan amount.
-// Ask user for Annual Percentage Rate (APR) (in percent).
-// Ask user for loan duration (in years).
-// Calculate for monthly payment.
-// Print monthly payment to console.
+calculateMonthlyPayment(loanAmount, monthlyInterestRate, loanDurationInMonths);
+prompt(`Your monthly payment for a loan of $${loanAmount} with an APR of ${annualPercentageRate * 100}% for ${loanDurationInYears} years is:`);
+prompt(`$${monthlyPayment.toFixed(2)}`);
